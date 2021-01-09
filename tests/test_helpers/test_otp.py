a5241a1209157246902739ea60c0660abd6f4e30
alfo1995 <damelioalfonso@hotmail.com>
2021-01-09 18:20:24 +0100

    tests: added missing test to reach coverage threshold

--- tests/test_helpers/test_otp.py
+++ tests/test_helpers/test_otp.py
@@ -24,10 +24,26 @@ from immuni_otp.core import config
 from immuni_otp.core.managers import managers
 from immuni_otp.helpers.otp import store
 
-_OTP = "an_otp_code"
+_OTP = "59FU36KR46"
 _OTP_SHA = sha256(_OTP.encode("utf-8")).hexdigest()
-_OTP_DATA = OtpData(symptoms_started_on=date(year=2020, month=12, day=10))
-_OTP_DATA_SERIALIZED = json.dumps({"symptoms_started_on": "2020-12-10"})
+_OTP_DATA = OtpData(
+    symptoms_started_on=date(year=2020, month=12, day=10), id_test_verification=None
+)
+_OTP_DATA_SERIALIZED = json.dumps(
+    {"id_test_verification": None, "symptoms_started_on": "2020-12-10",}
+)
+
+_CUN = "b39e0733843b1b5d7c558f52f117a824dc41216e0c2bb671b3d79ba82105dd94"
+_CUN_DATA = OtpData(
+    symptoms_started_on=date(year=2020, month=12, day=10),
+    id_test_verification="2d8af3b9-2c0a-4efc-9e15-72454f994e1f",
+)
+_CUN_DATA_SERIALIZED = json.dumps(
+    {
+        "id_test_verification": "2d8af3b9-2c0a-4efc-9e15-72454f994e1f",
+        "symptoms_started_on": "2020-12-10",
+    }
+)
 
 
 async def test_store_success() -> None:
@@ -41,6 +57,17 @@ async def test_store_success() -> None:
     assert 0 < ttl <= config.OTP_EXPIRATION_SECONDS
 
 
+async def test_cun_store_success() -> None:
+    key = f"~otp:{_CUN}"
+    assert await managers.otp_redis.get(key=key) is None
+    await store(otp=_CUN, otp_data=_CUN_DATA)
+    actual = await managers.otp_redis.get(key=key)
+    expected = _CUN_DATA_SERIALIZED
+    assert actual == expected
+    ttl = await managers.otp_redis.ttl(key=key)
+    assert 0 < ttl <= config.OTP_EXPIRATION_SECONDS
+
+
 async def test_store_failure_on_existent_key() -> None:
     key = f"~otp:{_OTP_SHA}"
     did_set = await managers.otp_redis.set(
@@ -49,3 +76,13 @@ async def test_store_failure_on_existent_key() -> None:
     assert did_set is True
     with raises(OtpCollisionException):
         await store(otp=_OTP, otp_data=_OTP_DATA)
+
+
+async def test_cun_store_failure_on_existent_key() -> None:
+    key = f"~otp:{_CUN}"
+    did_set = await managers.otp_redis.set(
+        key=key, value=_CUN_DATA_SERIALIZED, exist=StringCommandsMixin.SET_IF_NOT_EXIST
+    )
+    assert did_set is True
+    with raises(OtpCollisionException):
+        await store(otp=_CUN, otp_data=_CUN_DATA)
