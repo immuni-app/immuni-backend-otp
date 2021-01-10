2735ca5cd20f94163a860244f29162ea46321c2f
alfo1995 <damelioalfonso@hotmail.com>
2021-01-10 12:58:57 +0100

    style: checks test

--- tests/test_helpers/test_otp.py
+++ tests/test_helpers/test_otp.py
@@ -27,14 +27,10 @@ from immuni_otp.helpers.otp import store
 _OTP = "59FU36KR46"
 _OTP_SHA = sha256(_OTP.encode("utf-8")).hexdigest()
 _OTP_DATA = OtpData(
-    id_test_verification=None,
-    symptoms_started_on=date(year=2020, month=12, day=10),
+    id_test_verification=None, symptoms_started_on=date(year=2020, month=12, day=10),
 )
 _OTP_DATA_SERIALIZED = json.dumps(
-    {
-        "id_test_verification": None,
-        "symptoms_started_on": "2020-12-10",
-    }
+    {"id_test_verification": None, "symptoms_started_on": "2020-12-10", }
 )
 
 _CUN = "b39e0733843b1b5d7c558f52f117a824dc41216e0c2bb671b3d79ba82105dd94"
